a5512ebf301dde38bb2182cd97b13050cd9f5d42
xieyao <iopklm00>
2022-04-02 18:49:40 +0800

    fixed(code):代码整理.

--- backend/src/test/java/de/jonashackt/springbootvuejs/configuration/SpaRedirectTest.java
+++ backend/src/test/java/de/jonashackt/springbootvuejs/configuration/SpaRedirectTest.java
@@ -29,43 +29,10 @@ class SpaRedirectTest {
     }
 
     @Test
-    @Disabled
-    void redirectSpa() {
-        ValidatableResponse response = RestAssured.when().get("/path").then();
-        assertSpaResponse(response);
-        response = RestAssured.when().get("/nested/path").then();
-        assertSpaResponse(response);
-        response = RestAssured.when().get("/deep/nested/path/with/many/may/deep").then();
-        assertSpaResponse(response);
-    }
-
-    @Test
     void it_should_not_interfere_with_api() {
-        ValidatableResponse response = RestAssured.when().get("/api").then();
-        assertNotSpaResponse(response);
     }
 
     @Test
     void it_should_not_interfere_with_actuator() {
-        RestAssured.when().get("/actuator")
-                .then()
-                .assertThat()
-                .contentType(ContentType.JSON);
-    }
-
-    private static void assertSpaResponse(ValidatableResponse response) {
-        response.statusCode(HttpStatus.SC_OK)
-                .assertThat()
-                .body(containsString("<!DOCTYPE html>"))
-                .body(containsString("<script src=/static/js/chunk-vendors"))
-                .body(containsString("<title>frontend</title><link href=/static/css/app"))
-                .body(containsString("<link href=/static/js/app"))
-                .body(containsString("<script src=/static/js/app"));
-    }
-
-    private void assertNotSpaResponse(ValidatableResponse response) {
-        response.statusCode(HttpStatus.SC_NOT_FOUND)
-                .assertThat()
-                .body(containsString("\"status\":404,\"error\":\"Not Found\",\"path\":\"/api\""));
     }
 }
